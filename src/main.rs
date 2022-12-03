c7f92db81ed6133f41ebc0a49872260a75340465
toywsl <toywsl@motomoto.com>
2022-12-03 20:43:10 +0700

    add new items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use regex::Regex;
+
 fn main() {
     println!("Hello, world!");
+    println!("Hello, Jupiter!");
+
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
